a167f332b45706b5077bd3c7e55c65c98a9b206f
Juan Baltierrez <juanbal2808@gmail.com>
2021-03-15 00:25:27 -0500

    Api url modified 1/3

--- src/helper/getData.js
+++ src/helper/getData.js
@@ -1,5 +1,5 @@
 export const getData = async (id) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=ae529f25215a1fd78d6086852aaa6579&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=ae529f25215a1fd78d6086852aaa6579&units=metric`;
     const resp = await fetch(url);
 
     const data = await resp.json();
